80bae42827aaadfdeebe5de3aba571741be761f2
saffalou <testingbeagle@gmail.com>
2024-09-18 15:20:32 +1000

    moved file to correct location and added comment

--- Tools_and_games/text_length_generator.py
+++ Tools_and_games/text_length_generator.py
@@ -1,12 +1,13 @@
 import random
 import string
 
+# this is simple text generator that allows you to nominate a string length and then generates a random string of that length
 def get_random_string(length):
     # choose from all lowercase letter
     letters = string.ascii_lowercase
     result_str = ''.join(random.choice(letters) for i in range(length))
     print("Random string of length", length, "is:", result_str)
 
-get_random_string(8)
-get_random_string(6)
-get_random_string(4)
+get_random_string(35)
+#get_random_string(6)
+#get_random_string(4)
